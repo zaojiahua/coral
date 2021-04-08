995530d34dfe1c8b7389d3c793d9f70c3feb997e
darr_en1 <714970206@qq.com>
2021-04-08 18:15:08 +0800

    fix bug: cabinet_id 变更

--- app/v1/tboard/__init__.py
+++ app/v1/tboard/__init__.py
@@ -4,7 +4,7 @@ from app.v1.tboard.model.tboard import TBoard
 from app.config.ip import HOST_IP
 
 def tboard_init():
-    response = request(url=tboard_url.format(f"?finished_flag=False&fields=id&device__cabinet={int(HOST_IP.split('.')[-2])}"))
+    response = request(url=tboard_url.format(f"?finished_flag=False&fields=id&device__cabinet={int(HOST_IP.split('.')[-1])}"))
     unfinished_tboard_list = [str(tboard.get("id")) for tboard in response.get("tboards") if tboard]
     for tboard_id in unfinished_tboard_list:
         try:

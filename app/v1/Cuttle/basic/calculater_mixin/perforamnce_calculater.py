473b5d03c840e7abe0e3598ccd2a8c047b32ea23
Maoyuanjie <maoyuanj@126.com>
2021-05-07 18:35:06 +0800

    滑动检测起点，由用户框选区域是否全黑，改为框选中心点+-30pixel验证

--- app/v1/Cuttle/basic/calculater_mixin/perforamnce_calculater.py
+++ app/v1/Cuttle/basic/calculater_mixin/perforamnce_calculater.py
@@ -22,7 +22,13 @@ class PerformanceMinix(object):
     def start_point_with_icon(self, exec_content):
         # 方法名字尚未变更，此为滑动检测起点的方法
         data = self._validate(exec_content, PerformanceSchema)
-        performance = PerformanceCenter(self._model.pk, data.get("icon_areas"), data.get("refer_im"),
+        # 获取用户的icon选区，按中心点重建边长为30的正方形选区
+        x1 = data.get("icon_areas")[0][0]
+        y1 = data.get("icon_areas")[0][1]
+        x2 = data.get("icon_areas")[0][2]
+        y2 = data.get("icon_areas")[0][3]
+        icon_areas = [(x1 + x2) / 2 - 15, (y1 + y2) / 2 - 15, (x1 + x2) / 2 + 15, (y1 + y2) / 2 + 15]
+        performance = PerformanceCenter(self._model.pk, [icon_areas], data.get("refer_im"),
                                         data.get("areas")[0], data.get("threshold", 0.99),
                                         self.kwargs.get("work_path"), self.dq, bias=SWIPE_BIAS)
         return performance.start_loop(self._black_field)

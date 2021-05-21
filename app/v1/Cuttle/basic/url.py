42b2d4d1b9a85f5bad7fdd50316db008d6a1d816
Maoyuanjie <maoyuanj@126.com>
2021-05-21 18:17:36 +0800

    1变更引用方式  消除报错 2dockerfile添加换源code，避免反复timeout

--- app/v1/Cuttle/basic/url.py
+++ app/v1/Cuttle/basic/url.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 basic = Blueprint('basic', __name__)
 
-from .basic_views import test_icon_exist, test_position, test_position_fixed, ocr_test
+from app.v1.Cuttle.basic import basic_views

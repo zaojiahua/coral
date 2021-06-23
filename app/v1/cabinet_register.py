67bd817d76665e3e50afbddcfb222fc37915892e
Maoyuanjie <maoyuanj@126.com>
2021-06-23 16:21:05 +0800

    add id when register cabinet

--- app/v1/cabinet_register.py
+++ app/v1/cabinet_register.py
@@ -11,7 +11,8 @@ def cabinet_register():
         jsdata = {"cabinet_name": f"I'M {cabinet_id}#-({CORAL_TYPE})",
                   "ip_address": HOST_IP,
                   "is_delete": False,
-                  "type": f"Tcab_{CORAL_TYPE}"}
+                  "type": f"Tcab_{CORAL_TYPE}",
+                  "id":cabinet_id}
     except:
         cabinet_id = REEF_IP.split(".")[-1]
         jsdata = {"cabinet_name": "default_name"}

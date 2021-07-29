d766ffa27ce186d68e63099157fdde8ffd9fda16
Maoyuanjie <maoyuanj@126.com>
2021-07-29 18:12:21 +0800

    1. 增加下载job timeout时间 2. 增大线程池

--- app/v1/tboard/config/setting.py
+++ app/v1/tboard/config/setting.py
@@ -5,5 +5,5 @@ from app.config.setting import PROJECT_SIBLING_DIR
 # 运行时，job 资源文件存放地址
 TBOARD_PATH = os.path.join(PROJECT_SIBLING_DIR, "Tboard") + os.sep
 # 线程池大小
-MAX_CONCURRENT_NUMBER = 128
+MAX_CONCURRENT_NUMBER = 256
 STOP_DUT_MAX_ELAPSED_TIME = 30

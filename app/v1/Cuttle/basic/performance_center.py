b81f604e2c649c975aa926f3d65a9711949f21bd
zaojiahua <2933682586@qq.com>
2022-04-25 16:32:29 +0800

    修改5d

--- app/v1/Cuttle/basic/performance_center.py
+++ app/v1/Cuttle/basic/performance_center.py
@@ -260,7 +260,7 @@ class PerformanceCenter(object):
         # use_icon_scope为true时裁剪snap图中真实icon出现的位置
         # use_icon_scope为false时裁剪snap图中refer中标记的configArea选区大致范围
         picture = None
-        max_retry_time = 5
+        max_retry_time = 10
         while max_retry_time >= 0:
             if len(self.back_up_dq) >= 3:
                 try:

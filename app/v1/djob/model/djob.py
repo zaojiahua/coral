919131531f16dc4ddbece0dedfa229d461f5e61e
zaojiahua <2933682586@qq.com>
2022-06-20 15:49:27 +0800

    性能测试推送图片

--- app/v1/djob/model/djob.py
+++ app/v1/djob/model/djob.py
@@ -235,10 +235,10 @@ class DJob(BaseModel):
                 all_files.append(file_path)
 
             # 控制一次传输图片的数量
-            step = 2
+            step = 20
             for i in range(0, len(all_files), step):
                 files = [('files', (os.path.split(file_path)[-1], open(file_path, 'rb'), 'file'))
-                         for file_path in all_files[i, i + step]]
+                         for file_path in all_files[i: i + step]]
 
                 response = request(method="POST", url=rds_performance_pic, data={'rds': rds_id}, files=files)
                 print('performance pic', response)

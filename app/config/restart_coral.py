2c1aa1dfd732744e5c14c277586b37d524735b6b
zaojiahua <2933682586@qq.com>
2022-10-19 12:45:31 +0800

    内存不足，重启容器

--- app/config/restart_coral.py
+++ app/config/restart_coral.py
@@ -3,5 +3,5 @@ import subprocess
 
 # 重启coral容器
 def restart_coral():
-    subprocess.Popen('docker restart machexec_container',
+    subprocess.Popen('kill 1',
                      shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)

2f9453fd6feccf71393539bfec2a8b476a08b898
zaojiahua <2933682586@qq.com>
2022-11-28 09:37:05 +0800

    更新帧率脚本

--- performance_script/atrace_s/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-curPath = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(curPath)
-from . import *
--- performance_script/run.py
+++ performance_script/run.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-print(sys.path)
 import tracer
 
 if __name__ == '__main__':
